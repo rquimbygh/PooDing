6af4427ed06d918a4a86859e8871487a3b07589d
Rachel Quimby <rquimby@mills.edu>
2014-01-06 17:56:22 -0500

    Initial commit from local git

---
+++ PooDing/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PooDing
+//
+//  Created by Rachel Quimby on 1/6/14.
+//  Copyright (c) 2014 Rachel Quimby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
